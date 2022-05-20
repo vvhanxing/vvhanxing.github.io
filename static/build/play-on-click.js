d55fb469baa438c34827a3a03f4a1fb7867d74bf
vvhanxing <2308518201@qq.com>
2022-05-20 17:15:36 +0800

    fix click

--- static/build/play-on-click.js
+++ static/build/play-on-click.js
@@ -1,18 +1,57 @@
-/* global AFRAME */
+// /* global AFRAME */
+// AFRAME.registerComponent('play-on-click', {
+//   init: function () {
+//     this.onClick = this.onClick.bind(this);
+    
+//   },
+//   play: function () {
+//     window.addEventListener('click', this.onClick);
+//   },
+//   pause: function () {
+//     window.removeEventListener('click', this.onClick);
+//   },
+//   onClick: function (evt) {
+//     var videoEl = this.el.getAttribute('material').src;
+//     if (!videoEl) { return; }
+//     this.el.object3D.visible = true;
+//     videoEl.play();
+//   }
+// });
+
+
 AFRAME.registerComponent('play-on-click', {
   init: function () {
-    this.onClick = this.onClick.bind(this);
+
+
+
   },
-  play: function () {
-    window.addEventListener('click', this.onClick);
-  },
-  pause: function () {
-    window.removeEventListener('click', this.onClick);
-  },
-  onClick: function (evt) {
+
+
+
+  update: function () {
+
+
+
     var videoEl = this.el.getAttribute('material').src;
     if (!videoEl) { return; }
     this.el.object3D.visible = true;
     videoEl.play();
-  }
+    
+
+    }
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
